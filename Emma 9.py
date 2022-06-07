635cd3e7cb9f868d3f024c07b09d6fc0eb790e98
EmmanuelCodemaster <106280450+EmmanuelCodemaster@users.noreply.github.com>
2022-06-07 20:03:59 -0300

    Emmanuel ejercicio 9
    9- función p1, retorna el producto de los 2 primero más el 3er parámetros

--- Emma 9.py
+++ Emma 9.py
@@ -1,3 +1,4 @@
+#9- función p1, retorna el producto de los 2 primero más el 3er parámetros
 a=float(input("Ingrese número: ",))
 b=float(input("Ingrese número: ",))
 c=float(input("Ingrese número: ",))
