dfb268f8610f7050e526ea71a59b6b326d5254e7
EmmanuelCodemaster <106280450+EmmanuelCodemaster@users.noreply.github.com>
2022-06-09 17:43:26 -0300

    Ingrsa 2 valores

--- fing2i.py
+++ fing2i.py
@@ -1,13 +1,8 @@
 def ing2i ():
     a= int( input("Ingrese el primer número entero: "))
     b= int( input("Ingrese el segundo número entero:  "))
-<<<<<<< Updated upstream
     return [a,b]
 
 
     
-=======
-    return (a,b)
 
-
->>>>>>> Stashed changes
