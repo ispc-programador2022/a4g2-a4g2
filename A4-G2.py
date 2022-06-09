65ccec52c57aa8e1b693cef63360a05d1ed56b40
krla2022 <karlaconty@gmail.com>
2022-06-09 12:39:34 -0300

    Delete funcion_suma.py

---
+++ A4-G2.py
@@ -0,0 +1,10 @@
+from FUNCIONES.cartel import cartel
+from FUNCIONES.ing2i import ing2i
+from FUNCIONES.suma import suma
+from FUNCIONES.resta import resta
+from FUNCIONES.modulo import modulo
+
+print(cartel())
+
+enteros= ing2i()
+print("Los enteros ingresados: ", enteros[0], "y", enteros[1])
