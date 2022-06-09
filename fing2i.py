82508953eea1de4fc1fecca4655c9142a1098fb7
EmmanuelCodemaster <106280450+EmmanuelCodemaster@users.noreply.github.com>
2022-06-09 17:40:04 -0300

    producto de 2 números + 1 tercero

--- fing2i.py
+++ fing2i.py
@@ -1,7 +1,13 @@
 def ing2i ():
     a= int( input("Ingrese el primer número entero: "))
     b= int( input("Ingrese el segundo número entero:  "))
+<<<<<<< Updated upstream
     return [a,b]
 
 
     
+=======
+    return (a,b)
+
+
+>>>>>>> Stashed changes
---
+++ funcion9.py
@@ -0,0 +1,8 @@
+from producto import producto
+
+def funcion9(a,b,c):
+    return producto(a,b)+c
+
+
+
+    
