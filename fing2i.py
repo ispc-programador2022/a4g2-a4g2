78956db54d79b652d9084bbfecfd61e441b53bc9
nataliacuestas20 <106284015+nataliacuestas20@users.noreply.github.com>
2022-06-10 01:28:19 -0300

    ingreso de 2 valores enteros

--- fing2i.py
+++ fing2i.py
@@ -1,3 +1,5 @@
+#función ing2i, debe permitir el ingreso de 2 valores enteros
+
 def ing2i ():
     a= int( input("Ingrese el primer número entero: "))
     b= int( input("Ingrese el segundo número entero:  "))
