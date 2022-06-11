2879c58f8ffa1ef217cdf47a685afc22d3fbbf26
MarceloGustavoCordoba <marcelo.cordoba@live.com.ar>
2022-06-11 22:28:41 +0000

    funcion fing2s

---
+++ fing2s.py
@@ -0,0 +1,6 @@
+#función ing2i, debe permitir el ingreso de 2 valores string
+
+def ing2i ():
+    a= str( input("Ingrese el primer valor string: "))
+    b= str( input("Ingrese el segundo valor string:  "))
+    return [a,b]
