6db2412375d55c24f28521480ffda18416417c69
krla2022 <101228469+krla2022@users.noreply.github.com>
2022-06-13 20:31:37 -0300

    Suma genrnd

--- sumagenrd.py
+++ sumagenrd.py
@@ -10,3 +10,4 @@ def sumgen():
             total+= lista[i] + lista[j]
     return total
 ## Recorro la lista asignando i y j para sacar la suma de las combinanciones posibles de los numeros aleatorios tomados de a dos
+##Ejercicio realizado con colaboración de Chacón Claudio
