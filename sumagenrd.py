c7814d097fad16b02453827ac8382ef33d1987e8
krla2022 <101228469+krla2022@users.noreply.github.com>
2022-06-13 16:22:18 -0300

    Issue #17
    Colaborador: Contreras Carla
    Ejercicio 13- función que devuelva la suma de las combinaciones posibles de los números generados
    por la función genrnd tomados de a dos

--- sumagenrd.py
+++ sumagenrd.py
@@ -1,4 +1,6 @@
+## Llamo a la funcion genrnd de 50 num aleatorios
 import genrdm
+
 
 def sumgen():
     lista=genrdm()
@@ -7,3 +9,4 @@ def sumgen():
         for j in range(i+1,len(lista)):
             total+= lista[i] + lista[j]
     return total
+## Recorro la lista asignando i y j para sacar la suma de las combinanciones posibles de los numeros aleatorios tomados de a dos
