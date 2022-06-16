2ca5bed69fc295ffa4a036999d35fc337bce76f5
nataliacuestas20 <106284015+nataliacuestas20@users.noreply.github.com>
2022-06-16 18:03:35 -0300

    Update progen.py

---
+++ progen.py
@@ -0,0 +1,8 @@
+# 14- función que devuelva el producto de las combinaciones posibles de los números generados por la función genrnd tomados de a dos.
+def progen():
+    lista=genrdm()
+    total=0
+    for i in range(0,len(lista)-1):
+        for j in range(i+1,len(lista)):
+            total*= lista[i] * lista[j]
+    return total
