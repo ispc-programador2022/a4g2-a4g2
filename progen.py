957c54a8226483c0036be81a6e52c00e5c166d74
nataliacuestas20 <106284015+nataliacuestas20@users.noreply.github.com>
2022-06-16 18:10:21 -0300

    Update progen.py

--- progen.py
+++ progen.py
@@ -1,5 +1,7 @@
 # 14- función que devuelva el producto de las combinaciones posibles de los 
 # números generados por la función genrnd tomados de a dos.
+
+## Llamo a la funcion genrnd de 50 num aleatorios
 def progen():
     lista=genrdm()
     total=0
@@ -7,3 +9,5 @@ def progen():
         for j in range(i+1,len(lista)):
             total*= lista[i] * lista[j]
     return total
+## Recorro la lista asignando i y j para sacar la suma de las combinanciones posibles de los numeros aleatorios tomados de a dos
+##Ejercicio realizado con colaboración de Chacón Claudio
