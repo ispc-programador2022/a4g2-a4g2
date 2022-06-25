fe33623004b0575c273c837d0f493ee54c03a1dc
nataliacuestas20 <106284015+nataliacuestas20@users.noreply.github.com>
2022-06-24 23:38:06 -0300

    Update varianza.py

--- varianza.py
+++ varianza.py
@@ -1,40 +1,11 @@
+# 19- función que calcule la varianza del vector obtenido en genrnd.
 
-import random
-import statistics
-
-num_rand = []
-med_num_rand = []
-mediana_num_rand =[]
-var_num_rand=[]
-
-def genrnd(a, b):
-    for i in range(50):
-        num_rand.append(random.uniform(a,b))
-      
-def media(c):
-    med_num_rand = statistics.mean(c)
-    return med_num_rand
-
-def mediana(d):
-    mediana_num_rand = statistics.median(d)
-    return mediana_num_rand
-
-def rango(e):
-    rango_num_rand = len(e)
-    return rango_num_rand
-
-def varianza(f):
-    var_num_rand = statistics.variance(f)
-    return var_num_rand
-        
-genrnd(100, 200)
-
-print(num_rand)
-print(media(num_rand))
-print(mediana(num_rand))
-print(rango(num_rand))
-print(varianza(num_rand))
-
-
-#El programa calcula la mediana de los valores flotantes almacenados en la lista
-#num_rand. La media es almacenada en la variable mediana_num_rand
+def Vargen (numalea):
+    list= numalea
+    larg= len (list)
+    media= sum(list)/larg
+    desvia= [(x - media) ** 2 for x in list]
+    varianza = sum (desvia) / larg
+    return varianza
+    
+    #Colaboracion Carla Contreras
