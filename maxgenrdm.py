26bf29bae7f721170018911167012318bf442dcb
MarceloGustavoCordoba <marcelo.cordoba@live.com.ar>
2022-06-28 21:23:13 +0000

    funcion maxgenrdm corregida a raiz de feedback

--- maxgenrdm.py
+++ maxgenrdm.py
@@ -1,5 +1,3 @@
-from genrdm import genrdm #importo el modulo
-
-def maxgenrdm():# defino la funcion
-    numeros=genrdm()#llamo a genrdm que devuelve lista de 50 valores aleatorios
-    return max(numeros)#uso la funcion max para identificar el valor maximo de la lista y lo devuelvo al main
+def maxgenrdm(listax):# defino la funcion
+    numeros=max(listax)#uso la funcion max para identificar el valor maximo de la lista y lo devuelvo al main
+    return numeros#retorno el resultado
