59ac8d76dbee6e23aef99079306955d7b30c1f42
nataliacuestas20 <106284015+nataliacuestas20@users.noreply.github.com>
2022-06-27 23:47:41 -0300

    Función 14: Devuelva el producto de las combinaciones posibles

--- progen.py
+++ progen.py
@@ -7,7 +7,7 @@ def progen(listax):
     total=0
     for i in range(0,len(lista)-1):
         for j in range(i+1,len(lista)):
-            total*= lista[i] * lista[j]
+            total+= lista[i] * lista[j]
     return total
 ## Recorro la lista asignando i y j para sacar la suma de las combinanciones posibles de los numeros aleatorios tomados de a dos
 ##Ejercicio realizado con colaboración de Chacón Claudio
