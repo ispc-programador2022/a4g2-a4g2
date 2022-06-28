cc3214839cec9c0321e10d2a933b14d35262f85f
MarceloGustavoCordoba <marcelo.cordoba@live.com.ar>
2022-06-28 03:42:30 +0000

    función genrnd que retorna una lista con 500.000.000.000.000.000 números aleatorios

--- G2-A4.py
+++ G2-A4.py
@@ -11,21 +11,25 @@ from modulo import modulo
 from potencia import potencia
 from raiz import raiz
 from funcion9 import funcion9
-from Ejercicio 10 import Ejercicio 10 #Quitar espacios del nombre del archivo py
+#from Ejercicio 10 import Ejercicio 10 #Quitar espacios del nombre del archivo py
 from funcion11 import funcion11
 from genrdm import genrdm 
 from sumagenrd import sumgenr
-from progen import progen ## modificar  funcion asignarle una lista x ejemplo def progenr(numalea): lista= numalea
+from progen import progen 
 from maxgenrdm import maxgenrdm ## modificar funcion sacar el import genrdm, asignar (listax) numeros=listax
 from minigen import min_genrnd
 from medianadelvector import mediardm
-from Mediana del vector import medianadelvector #Quitar espacios del nombre del archivo py
+#from Mediana del vector import medianadelvector #Quitar espacios del nombre del archivo py
+from varianza import Vargen
+from genrdmgigante import genrdmgigante
+from var50m import Vargen50m
+from min50m import min_50m
 
 ## asignamos los valores para las operaciones#
 enteros= ing2i()
 str= ing2s()
 numrand=genrdm()
-#NR50M= Funcion q genere los 50 ml random()
+nr50m= genrdmgigante()
 print("Operación Suma de Enteros: ", enteros[0], "+", enteros[1], "=", suma(enteros[0],enteros[1])) ##1- función suma
 print("Operación Resta de Enteros: ", enteros[0], "-", enteros[1], "=", resta(enteros[0],enteros[1])) ##2- función resta
 print("Operación Producto de Enteros: ", enteros[0], "*", enteros[1], "=", producto(enteros[0],enteros[1])) ##2- función resta
@@ -41,25 +45,26 @@ print("Operación Resta de los 2 Enteros por el Tercer Parametro es= ", funcion1
 
 print("Numeros aleatorios: ", numrand)##12- función genrnd
 print("Suma de las Combinaciones posibles Genrnd tomados de a dos: ", sumgenr(numrand))##13- función que devuelva la suma 
-#print("Producto de las Combinaciones posibles Genrnd tomados de a dos: ", progen(numrand))##14- función que devuelva el producto
+print("Producto de las Combinaciones posibles Genrnd tomados de a dos: ", progen(numrand))##14- función que devuelva el producto
 
 ## iniciar cuenta tiempo ejecucion ejercicio 29
 print("Media de Los 50 Numeros Aleatorios: ", mediardm(numrand))##16- función que calcule la media    
-print("Mediana de Los 50 Numeros Aleatorios: ", medianadelvector(numrand))
+#print("Mediana de Los 50 Numeros Aleatorios: ", medianadelvector(numrand))
 #print("Rango de Los 50 Numeros Aleatorios: ", rangogen(numrand))
 print("La Varianza de Los 50 Numeros Aleatorios: ", Vargen(numrand)) ##19- función que calcule la varianza
 print("El minimo del vector de Los 50 Numeros Aleatorios: ", min_genrnd(numrand))##20- función que calcule devuelva el mínimo
 #print("El maximo del vector de Los 50 Numeros Aleatorios: ", maxgenrdm(numrand))
+
 ## Finalizar cuenta tiempo ejecucion ejercicio 29
 
 ## iniciar cuenta tiempo ejecucion ejercicio 30
 
-#print("La media de los 50M Numeros Aleatorios : ", media5m(NR50M))
-#print("La mediana de los 50M Numeros Aleatorios : ", median50m(NR50M))
-#print("Rango de Los 50M Numeros Aleatorios : ", rango50m(NR50M))
-#print("La Varianza de  Los 50M Numeros Aleatorios : ", Vargen50m(NR50M))
-#print("El minimo del vector de Los 50M Numeros aleatorios: ", min_50m(NR50M))
-#print("El maximo del vector de Los 50M Numeros aleatorios: ", maxgenr50m(NR50M))
+#print("La media de los 50M Numeros Aleatorios : ", media5m(nr50m))
+#print("La mediana de los 50M Numeros Aleatorios : ", median50m(nr50m))
+#print("Rango de Los 50M Numeros Aleatorios : ", rango50m(nr50m))
+print("La Varianza de  Los 50M Numeros Aleatorios : ", Vargen50m(nr50m))
+print("El minimo del vector de Los 50M Numeros aleatorios: ", min_50m(nr50m))
+#print("El maximo del vector de Los 50M Numeros aleatorios: ", maxgenr50m(nr50m))
 ## Finalizar cuenta tiempo ejecucion ejercicio 30
 #print("Tiempo de ejecucion Funciones 16 a 21: ", fin - inicio) #Ejercicio 29
 #print("Tiempo de ejecucion Funciones 22 a 28: ", fin1 - inicio1) #Ejericio 30
--- genrdmgigante.py
+++ genrdmgigante.py
@@ -1,14 +1,5 @@
 from random import randint
 
 def genrdmgigante():
-  nr = []
-  # ciclo configurado segun el requerimiento del ejercicio,
-  # se coloca comentado ya que el proceso consumo mas RAM de la disponible y falla.
-  #for x in range(0,10000000):
-    #b = [randint(0,5000) for i in range(50000000000)]
-    #nr.append(b)
-  # en su lugar se configura el mismo proceso con un numero de elementos menor:
-  for x in range(0,100):
-    b = [randint(0,5000) for i in range(5000)]
-    nr.append(b)
+  nr = [randint(0,5000) for i in range(5000)]
   return nr
